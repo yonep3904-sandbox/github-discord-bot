22276f3dcc09d61844600114e85825a6d38313af
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2026-03-18 18:59:53 +0900

    package.json のjestの設定を削除

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,20 @@
-export default {
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+
+  // Add this to handle path aliases defined in tsconfig.json
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@/(.*)$': '<rootDir>/$1',
   },
 };
+
+export default config;
