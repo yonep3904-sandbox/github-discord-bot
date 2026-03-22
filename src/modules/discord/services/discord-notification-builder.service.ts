15ae588726111b6d563d7c4a86b4a2c3ea2b2ba5
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2026-03-22 22:26:37 +0900

    手動通知型のBuilderを追加

--- src/modules/discord/services/discord-notification-builder.service.ts
+++ src/modules/discord/services/discord-notification-builder.service.ts
@@ -1,5 +1,9 @@
 import { Injectable, Logger } from '@nestjs/common';
-import type { NotificationEvent } from '@/types/internal/notification-event';
+import type {
+  NotificationEvent,
+  ManualNotificationContent,
+  GithubNotificationContent,
+} from '@/types/internal/notification-event';
 import type { DiscordNotificationPayload } from '@/types/external/discord';
 import { NotificationBuilderImpl } from '@/types/internal/notification-platform';
 
@@ -8,9 +12,38 @@ export class DiscordNotificationBuilder implements NotificationBuilderImpl<Disco
   private readonly logger = new Logger(DiscordNotificationBuilder.name);
 
   build(event: NotificationEvent): DiscordNotificationPayload {
-    const payload = {
-      content: `[dummy] New event: ${event.type}`,
+    switch (event.source) {
+      case 'manual':
+        return this.buildManualNotification(event.content);
+      case 'github':
+        return this.buildGithubNotification(event.content);
+    }
+  }
+
+  private buildManualNotification(
+    content: ManualNotificationContent,
+  ): DiscordNotificationPayload {
+    switch (content.type) {
+      case 'standard':
+        if (content.title == null) {
+          return {
+            content: content.message,
+          };
+        } else {
+          return {
+            content: `# ${content.title}\n${content.message}`,
+          };
+        }
+    }
+  }
+
+  private buildGithubNotification(
+    content: GithubNotificationContent,
+  ): DiscordNotificationPayload {
+    const dummy = {
+      content: `New pull request in ${content.repository} by ${content.actor}`,
     };
-    return payload;
+
+    return dummy;
   }
 }
