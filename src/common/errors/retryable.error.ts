f9c268ab844ac2d725699d16eeda70c6985de111
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2026-03-22 16:37:03 +0900

    消し忘れ

--- src/common/errors/retryable.error.ts
+++
@@ -1,9 +0,0 @@
-export class RetryableError extends Error {
-  constructor(
-    message: string,
-    public readonly cause?: unknown,
-  ) {
-    super(message);
-    this.name = 'RetryableError';
-  }
-}
